67901289d9a79395d12b941af962155088933fdf
TheDodo24 <dodogaming.info@gmail.com>
2016-11-27 17:41:34 +0100

    Update SinkIRC.java

--- SinkIRC/src/main/java/de/static_interface/sinkirc/SinkIRC.java
+++ SinkIRC/src/main/java/de/static_interface/sinkirc/SinkIRC.java
@@ -206,7 +206,7 @@ public class SinkIRC extends JavaPlugin {
         SinkLibrary.getInstance().registerCommand("say", new SayCommand(this, commandsConfig));
         SinkLibrary.getInstance().registerCommand("kick", new KickCommand(this, commandsConfig));
         SinkLibrary.getInstance().registerCommand("list", new ListCommand(this, commandsConfig));
-        SinkLibrary.getInstance().registerCommand("reload", new SetCommand(this, commandsConfig));
+        SinkLibrary.getInstance().registerCommand("test", new SetCommand(this, commandsConfig));
         SinkLibrary.getInstance().registerCommand("set", new ReloadCommand(this, commandsConfig));
         initialized = true;
     }
