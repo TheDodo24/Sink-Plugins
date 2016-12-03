7e0d15f596771af62a29c3503e9ad38de8c82311
TheDodo24 <dodogaming.info@gmail.com>
2016-12-03 13:35:01 +0100

    Update ReloadCommand.java

--- SinkIRC/src/main/java/de/static_interface/sinkirc/irc_command/ReloadCommand.java
+++ SinkIRC/src/main/java/de/static_interface/sinkirc/irc_command/ReloadCommand.java
@@ -10,32 +10,31 @@ import de.static_interface.sinklibrary.api.command.annotation.Usage;
 import de.static_interface.sinklibrary.api.configuration.Configuration;
 import de.static_interface.sinklibrary.util.BukkitUtil;
 
-@Description("Kann den Server reloaden")
 @Usage("")
-public class ReloadCommand extends IrcCommand {
+@Description("restartet den Ingameserver")
+public class RestartCommand extends IrcCommand {
 
-	public ReloadCommand(Plugin plugin, Configuration config) {
-        super(plugin, config);
-        getCommandOptions().setIrcOpOnly(true);
-    }
+	public RestartCommand(Plugin plugin, Configuration config) {
+		super(plugin, config);
+		getCommandOptions().setIrcOpOnly(true);
+	}
 
 	@Override
-	protected boolean onExecute(CommandSender sender, String label, String[] args){
+	protected boolean onExecute(CommandSender sender, String label, String[] args) {
 		if(args.length > 0) {
 			return false;
 		}
 		
-		sender.sendMessage("Der Server wird gleich neugeladen");
-		BukkitUtil.broadcastMessage("§4Achtung: §cDer Server wird gleich neugeladen!");
+		sender.sendMessage("Der Server wird gleich neugestartet!");
+		BukkitUtil.broadcastMessage("§4Achtung: §cDer Server wird gleich neugestartet!");
 		Bukkit.getScheduler().runTaskLater(SinkIRC.getPlugin(SinkIRC.class), new Runnable() {
 			
 			@Override
 			public void run() {
-				Bukkit.reload();
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "restart");
 			}
-		}, 5);
+		}, 100);
 		return true;
 	}
-	
 
 }
