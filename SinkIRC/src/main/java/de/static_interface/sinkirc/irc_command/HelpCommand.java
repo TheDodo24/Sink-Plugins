6ab5c3cf4b043a5ba64d0df50bf570e3676ebaa8
TheDodo24 <dodogaming.info@gmail.com>
2016-12-03 13:25:59 +0100

    Update HelpCommand.java

--- SinkIRC/src/main/java/de/static_interface/sinkirc/irc_command/HelpCommand.java
+++ SinkIRC/src/main/java/de/static_interface/sinkirc/irc_command/HelpCommand.java
@@ -3,7 +3,6 @@ package de.static_interface.sinkirc.irc_command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
-import de.static_interface.sinklibrary.api.command.SinkCommand;
 import de.static_interface.sinklibrary.api.command.annotation.Description;
 import de.static_interface.sinklibrary.api.command.annotation.Usage;
 import de.static_interface.sinklibrary.api.configuration.Configuration;
@@ -11,7 +10,7 @@ import de.static_interface.sinklibrary.api.sender.IrcCommandSender;
 
 @Description("Zeigt eine Hilfe über Befehle im IRC")
 @Usage("")
-public class HelpCommand extends SinkCommand {
+public class HelpCommand extends IrcCommand {
 
 	public HelpCommand(Plugin plugin, Configuration config) {
 		super(plugin, config);
@@ -26,16 +25,17 @@ public class HelpCommand extends SinkCommand {
 		IrcCommandSender sender = (IrcCommandSender) cs;
 		boolean isChanOp = de.static_interface.sinkirc.IrcUtil.isOp(sender.getUser().getBase());
 		
-		sender.sendMessage("     |~| IRC-Hilfe |~|     ");
-		sender.sendMessage("~help » Ruft diese Hilfe auf");
-		sender.sendMessage("~say [Naricht] » Chatverbindung in den Ingame-Chat");
-		sender.sendMessage("~list » Listet die Spieler Ingame auf");
-				   
-		if(isChanOp) {
-			sender.sendMessage("~exec [Kommand] » Führt einen Kommando als Konsole aus");
-			sender.sendMessage("~kick [Spieler] » Kickt einen Ingame-Spieler");
-			sender.sendMessage("~set [Variable] [Zustand] » Setzt eine Variable auf den Zustand");
-			sender.sendMessage("~srl » Reloaded den Ingame-Server");
+			sender.sendMessage("     |~| IRC-Hilfe |~|     \n"
+					+ "~help » Ruft diese Hilfe auf\n"
+					+ "~say [Naricht] » Chatverbindung in den Ingame-Chat\n"
+					+ "~list » Listet die Spieler Ingame auf");
+			if(isChanOp) {
+				sender.sendMessage("~exec [Kommand] » Führt einen Kommando als Konsole aus\n"
+						+ "~kick [Spieler] [Grund] » Kickt einen Ingame-Spieler mit dem Grund\n"
+						+ "~set [Variable] [Zustand] » Setzt eine Variable auf den Zustand\n"
+						+ "~srl » Reloaded den Ingame-Server\n"
+						+ "~bcmsg [Naricht] » Broadcastet die Naricht in den Ingamechat\n"
+						+ "~ikick [Spieler] [Grund] » Kickt einen Spieler auf dem IRC-Channel mit dem Grund");
 			}
 		
 		return true;
